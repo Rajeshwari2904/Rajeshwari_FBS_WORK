437fc563dc6aaaf2b8d7c8a269bf86c990528aaf
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-05-31 13:01:11 +0530

    Add files via upload

---
+++ Assig01.c
@@ -0,0 +1,12 @@
+//1. addition of 2 numbers.
+
+#include <stdio.h>
+void main()
+{
+	int no1, no2, result;
+	no1= 10;
+	no2= 20;
+	result = no1 + no2;
+	
+	printf("Addition of %d and %d is %d.", no1, no2, result);
+}
