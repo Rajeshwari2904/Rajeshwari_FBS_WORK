5ce452cd30e05fd3c35753d8fde71412e5ecdd4a
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-05-31 13:13:57 +0530

    Create Assign02.c

---
+++ Assign02.c
@@ -0,0 +1,14 @@
+//2. Write a C program to find the area of a circle
+
+#include<stdio.h>
+void main()
+{
+	int radius = 15;
+	float area;
+	
+	area = 3.14 * radius * radius;
+	
+	printf("Area of Circle is : %f",area);
+	
+}
+  
