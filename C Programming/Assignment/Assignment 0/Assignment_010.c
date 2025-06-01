267424c26e5d3395ef7e5fe9136c4efdbd863527
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-06-01 14:52:53 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 0/Assignment_010.c
@@ -0,0 +1,22 @@
+//10. Write a program to calculate the percentage of student based on marks of
+//   any 5 subjects.
+
+#include <stdio.h>
+void main()
+{
+	int science = 80, math = 90, history = 70, geo =75, hindi = 80;
+	int Sum, percentage;
+	
+	Sum = science +math +history +geo +hindi;
+	
+	printf("The total no. of marks are %d ",Sum);
+	
+	percentage = Sum /5;
+	
+	printf("\nPercentages = %d",percentage);
+	
+	
+	
+}
+     
+     
---
+++ C Programming/Assignment/Assignment 0/Assignment_09.c
@@ -0,0 +1,15 @@
+//9. Write a program to calculate area of triangle
+
+#include <stdio.h>
+void main()
+{
+	
+	int base= 5, height= 8;
+	float area;
+	
+	area = 0.5 * base* height;
+	
+	printf("Area of Triangle is %f", area);
+	
+	
+}
