14f12903ccf5a17effe5bd82fa14a59b2fe4f963
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-06-05 12:25:13 +0530

    Add files via upload

---
+++ C Programming/Assignment/Class_Tests/Test_1/Test_1_3.c
@@ -0,0 +1,14 @@
+// test 1
+
+//Print last 2 digit of Number eg.i/p: 223410 o/p :10.
+
+#include<stdio.h>
+
+void main()
+{
+	int no = 223410;
+	
+	int x = no % 100;
+
+	printf("Result : %d", x);
+}
