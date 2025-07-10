a16b15c522f5a9f1dd6f09b0672688d7d4967076
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-07-10 23:48:54 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 10/Assignment_10_c.c
@@ -0,0 +1,47 @@
+//1. Write a user define functions for :: c. mystrcmp
+
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+	char str1[20];
+	char str2[20];
+	printf("Enter 2 Strings: ");
+	scanf("%s\n", str1);
+	scanf("%s", str2);
+	
+	int res= mystrcmp(str1, str2);
+	
+	if(res== 0)
+	{
+		printf("Strings are Same");
+	}
+	
+	else if(res == 1)
+	{
+		printf("str1 %s is greater", str1);
+	}
+	else
+	{
+		printf("str2 %s is greater", str2);
+	}
+	
+}
+
+void mystrcmp(char* ptr1, char* ptr2)
+{
+	if(strlen(ptr1)!= strlen(ptr2))
+	    return 1;
+	    
+	int i= 0;
+	while(ptr1[i]!='\0')
+	{
+		if(ptr1[i]!= ptr2[i])
+		     return 1;
+		     
+	  i++;
+	}
+	
+	return 0;
+}
