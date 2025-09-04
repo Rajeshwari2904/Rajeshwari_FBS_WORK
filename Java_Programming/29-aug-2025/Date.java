0b45eb5b992f83704eb4a005149bf44d685b71f9
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-09-04 12:03:08 +0530

    Add files via upload

---
+++ Java_Programming/29-aug-2025/Date.java
@@ -0,0 +1,14 @@
+class Date
+{
+  int day, month,year;
+  String dow;
+}
+
+class Test
+{
+  public static void main(String[] args){
+     Date d1;
+     d1 = "Wed";
+     System.out.println(d1);
+  }
+}
