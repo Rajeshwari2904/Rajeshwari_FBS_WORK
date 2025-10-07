6a717977f5844283210b88fa450032171c695070
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-10-07 23:34:37 +0530

    Add files via upload

---
+++ Java_Programming/7-oct-2025/Arraylist.java
@@ -0,0 +1,22 @@
+package p1;
+
+import java.util.*;
+
+class Arraylist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList a1 = new ArrayList();
+		a1.add(10);
+		a1.add(30);
+		a1.add(5);
+		a1.add(20);
+		
+		System.out.println(a1);
+		System.out.println(a1.get(0));
+		
+
+	}
+
+}
