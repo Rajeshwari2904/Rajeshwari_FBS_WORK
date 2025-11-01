d4920c7716bbaac5ffa5011ddb4c8b8468f011fd
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-11-01 20:53:47 +0530

    Add files via upload

---
+++ Java_Programming/1-nov-2025/DemoStudents.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class DemoStudents implements payFees{
+	
+	public void payOnline() {
+		
+		System.out.println("Pay the fees by Cash");
+	}
+
+}
---
+++ Java_Programming/1-nov-2025/OfflineStudents.java
@@ -0,0 +1,19 @@
+package p1;
+
+public class OfflineStudents implements payFees{
+
+	@Override
+	public void payOnline() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void payOffline() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Student can pay fees offline");
+		
+	}
+
+}
