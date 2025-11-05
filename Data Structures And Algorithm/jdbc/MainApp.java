64c969ab3b5ecba4c3a331cf81680f8baede0f70
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-11-05 23:06:30 +0530

    Add files via upload

---
+++ Data Structures And Algorithm/jdbc/MainApp.java
@@ -0,0 +1,47 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//step 1: Load the Driver
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//protocol:subprotocol:DB- Specific information
+			//jdbc:mysql:
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/airline","root", "Rajeshwari@1234");
+			 
+			String query = "Select * from aircraft_info";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				System.out.println(rs.getString(1)+ " "+ rs.getString(2));
+			}
+			//System.out.println(rs.getString(1));
+			
+
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+	}
+
+}  // Resultset MetaDATA------>>>>Explore
+
+// there is a built in component row set jo use karne pe disconnected data milta hai....connected architecture
+// 
