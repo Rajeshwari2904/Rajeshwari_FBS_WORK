72b7d39cbf68bf0312c1890ddb5f1e408721d53c
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-11-17 23:35:13 +0530

    Add files via upload

---
+++ Data Structures And Algorithm/Trees/MainApp.java
@@ -0,0 +1,24 @@
+package com.tree;
+
+public class MainApp {
+	
+
+	public static void main(String[] args) {
+
+	  Node n1 =new Node("Shree");
+	  
+	  TreeDemo t1 = new TreeDemo();
+      
+   
+	  t1.insert("50");
+	  t1.insert("20");
+	  t1.insert("18");
+	  t1.insert("27");
+	  t1.insert("24");
+      
+	  t1.display();
+      
+      	
+	}
+
+}
