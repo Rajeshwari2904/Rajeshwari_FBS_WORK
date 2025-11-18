3a55f360a3caddcf7a926bbe1f7feac7af6f1a19
Rajeshwari Nalbalwar <159479695+Rajeshwari2904@users.noreply.github.com>
2025-11-18 23:54:36 +0530

    Add files via upload

---
+++ Data Structures And Algorithm/Queue/Main.java
@@ -0,0 +1,23 @@
+package com.raju;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		WeatherStation w1 = new WeatherStation();
+		
+		Mobile m1 = new Mobile();
+		Mobile m2 = new Mobile();
+		
+		w1.registered(m1);
+		w1.registered(m2);
+		
+		w1.tempchanged(2.5f); 
+		
+		w1.tempchanged(30.0f);
+		
+		w1.unregistered(m1);
+		w1.tempchanged(27.0f);
+	}
+
+}
